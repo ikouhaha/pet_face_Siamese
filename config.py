e6f94b96c122927310f70876446ae607a8380079
dennisauyeung <40794322+ikouhaha@users.noreply.github.com>
2022-07-03 17:26:05 +0800

    update dataset and ipynb

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config():
     training_dir = "./data/faces/training/"
     testing_dir = "./data/faces/testing/"
-    train_batch_size = 64
-    train_number_epochs = 100
+    train_batch_size = 32
+    train_number_epochs = 500
